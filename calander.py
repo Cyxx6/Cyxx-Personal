037251f6c6bb7d1dc84aaf946cf226e368a489c7
Cyxx <carwendy12@gmail.com>
2025-10-26 09:47:24 +0000

    initial comit

---
+++ calander.py
@@ -0,0 +1,18 @@
+"""import calender
+
+yy = 2025  # year
+mm = 12    # month
+
+# display the calendar
+
+print(calender.month(yy, mm))"""
+
+
+
+import calendar
+
+yy = 2025  # year
+mm = 10    # month
+
+# display the calendar
+print(calendar.month(yy, mm))
